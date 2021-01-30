654369c93bd9499aeaee783cb39481963579caaf
Pam Rodrigues <63943596+pvfpinho@users.noreply.github.com>
2021-01-30 01:27:55 -0700

    initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+package prove04;
+
+public class Main {
+    //creates a class
+
+    public static void main(String[] args) {
+        /* Main method to print my teammates name */
+
+            System.out.println("Hello, my name is Pam");
+
+
+        }
+    }
+
